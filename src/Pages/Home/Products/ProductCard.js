4513cfd52ce5e8fdcf7a3381fe9657a1e3c3bad4
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-31 19:35:03 +0600

    added react icons in products and team section

--- src/Pages/Home/Products/ProductCard.js
+++ src/Pages/Home/Products/ProductCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaStar } from "react-icons/fa";
 
 const ProductCard = ({ product }) => {
     const { image, name, price } = product;
@@ -7,6 +8,13 @@ const ProductCard = ({ product }) => {
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="" className='w-56 h-56' /></figure>
             <div className="card-body text-center">
+                <div className='flex justify-center'>
+                    <FaStar className='text-orange-600 w-4 h-4' />
+                    <FaStar className='text-orange-600 w-4 h-4' />
+                    <FaStar className='text-orange-600 w-4 h-4' />
+                    <FaStar className='text-orange-600 w-4 h-4' />
+                    <FaStar className='text-orange-600 w-4 h-4' />
+                </div>
                 <h2 className="text-2xl font-bold">{name}</h2>
                 <p className='text-orange-600 text-xl font-semibold'>${price}</p>
             </div>
--- src/Pages/Home/Team/Member.js
+++ src/Pages/Home/Team/Member.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaFacebook, FaTwitterSquare, FaLinkedin, FaInstagramSquare } from "react-icons/fa";
 
 const Member = ({ member }) => {
     const { image, name, designation } = member;
@@ -9,6 +10,12 @@ const Member = ({ member }) => {
             <div className="card-body text-center">
                 <h2 className="text-2xl font-bold">{name}</h2>
                 <p className="text-lg text-gray-600 font-semibold">{designation}</p>
+                <div className='flex justify-center gap-4'>
+                    <FaFacebook className='w-6 h-6 text-blue-900 hover:cursor-pointer'></FaFacebook>
+                    <FaTwitterSquare className='w-6 h-6 text-sky-600 hover:cursor-pointer'></FaTwitterSquare>
+                    <FaLinkedin className='w-6 h-6 text-blue-700 hover:cursor-pointer'></FaLinkedin>
+                    <FaInstagramSquare className='w-6 h-6 text-rose-600 hover:cursor-pointer'></FaInstagramSquare>
+                </div>
             </div>
         </div>
     );
