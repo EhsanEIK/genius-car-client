efed499f559a29dc4bb78bb6bb61f2b7a8c8c02b
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-31 14:20:03 +0600

    added Signin page

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layouts/Main";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Signin/Signin";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/signin',
+                element: <Login></Login>
             }
         ]
     }
