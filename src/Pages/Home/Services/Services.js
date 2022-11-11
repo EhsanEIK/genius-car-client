cae56ba2209f0939e227d97672b902aa5cb20295
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-11 18:18:08 +0600

    added search funtionality in Services section for searching the service using the service title

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,18 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [isAsc, setIsAsc] = useState(false);
     const [lowPrice, setLowPrice] = useState(false);
 
+    const searchRef = useRef();
+    const [search, setSearch] = useState('');
+
     // get all services from database
     const [services, setServices] = useState([]);
     useEffect(() => {
-        fetch(`https://genius-car-server-rouge.vercel.app/services?order=${isAsc ? 'asc' : 'desc'}&priceRange=${lowPrice ? 'low' : 'all'}`)
+        fetch(`https://genius-car-server-rouge.vercel.app/services?search=${search}&order=${isAsc ? 'asc' : 'desc'}&priceRange=${lowPrice ? 'low' : 'all'}`)
             .then(res => res.json())
             .then(data => setServices(data));
-    }, [isAsc, lowPrice])
+    }, [isAsc, lowPrice, search])
 
+    const handleSearch = () => {
+        setSearch(searchRef.current.value);
+    }
 
     return (
         <div className='my-32'>
@@ -21,9 +27,18 @@ const Services = () => {
             <p className='w-2/4 mx-auto text-center mb-10'>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
 
             <div className='flex justify-center my-10'>
+                <input ref={searchRef} className="input input-bordered input-sm mr-3" type="text" />
+                <button onClick={handleSearch} className='bg-gray-500 hover:bg-gray-600 text-white rounded-lg px-2 py-1'>
+                    Search</button>
+            </div>
+
+            <div className='flex justify-center my-10'>
                 <button className='bg-gray-500 hover:bg-gray-600 text-white rounded-lg px-2 py-1' onClick={() => setIsAsc(!isAsc)}>
                     {isAsc ? 'DESC' : 'ASC'}</button>
-                <button className='bg-lime-600 hover:bg-lime-700 text-white rounded-lg ml-3 px-2 py-1' onClick={() => setLowPrice(!lowPrice)}>
+                <button className='bg-lime-600 hover:bg-lime-700 text-white rounded-lg ml-3 px-2 py-1' onClick={() => {
+                    setLowPrice(!lowPrice)
+                    setSearch('');
+                }}>
                     {lowPrice ? 'SEE ALL' : 'BELOW $100'}</button>
             </div>
 
