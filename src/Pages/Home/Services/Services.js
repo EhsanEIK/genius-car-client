b0bd4e89a0435aeb58ac0f36c88e146bacf844a9
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-11 10:42:19 +0600

    added price query button and query in url for filtering the services on price

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
-    const [isAsc, setIsAsc] = useState(true);
+    const [isAsc, setIsAsc] = useState(false);
+    const [lowPrice, setLowPrice] = useState(false);
+
+    // get all services from database
     const [services, setServices] = useState([]);
     useEffect(() => {
-        fetch(`https://genius-car-server-rouge.vercel.app/services?order=${isAsc ? 'asc' : 'desc'}`)
+        fetch(`https://genius-car-server-rouge.vercel.app/services?order=${isAsc ? 'asc' : 'desc'}&priceRange=${lowPrice ? 'low' : 'all'}`)
             .then(res => res.json())
             .then(data => setServices(data));
-    }, [isAsc])
+    }, [isAsc, lowPrice])
 
 
     return (
@@ -16,10 +19,14 @@ const Services = () => {
             <p className="text-xl text-center text-orange-600 font-bold">Service</p>
             <h1 className="text-5xl text-center font-bold text-black my-6">Our Service Area</h1>
             <p className='w-2/4 mx-auto text-center mb-10'>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+
             <div className='flex justify-center my-10'>
                 <button className='bg-gray-500 hover:bg-gray-600 text-white rounded-lg px-2 py-1' onClick={() => setIsAsc(!isAsc)}>
                     {isAsc ? 'DESC' : 'ASC'}</button>
+                <button className='bg-lime-600 hover:bg-lime-700 text-white rounded-lg ml-3 px-2 py-1' onClick={() => setLowPrice(!lowPrice)}>
+                    {lowPrice ? 'SEE ALL' : 'BELOW $100'}</button>
             </div>
+
             <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 mb-14'>
                 {
                     services.map(service => <ServiceCard
